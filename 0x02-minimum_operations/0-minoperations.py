9722df1bd8abccedb5ddb870ec5185faf48ba1dd
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-11 18:34:59 +0100

    Minimum operation created

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,18 +9,15 @@ def minOperations(n: int) -> int:
     Calculate the minimum number of operations to achieve `n` 'H' characters
     in the file.
     '''
-    res: int = 0;
-
-    
     if n <= 1 or type(n) is not int:
         return 0
-    
-    i = 2
-    while i <= n:
-        while n % i == 0:
-            res += i
-            n = n // i
 
-        i += 1
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
 
-    return res
+    for i in range(2, n + 1):
+        for d in range(2, i + 1):
+            if i % d == 0:
+                dp[i] = min(dp[i], dp[d] + i // d)
+
+    return dp[n]
