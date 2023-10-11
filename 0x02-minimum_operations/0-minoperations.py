dac547e1735602f0430e5960d2c11a96ca047011
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-11 19:14:53 +0100

    Alternative way to achieve mimOperation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,17 +12,26 @@ def minOperations(n: int) -> int:
     res = 0
 
     if n <= 1 or type(n) is not int:
-        return res
-
+        return res  # No new operations added
+    '''
+    can be achieve by factorizing the number and sum up its prime factors
     i = 2
     while i * i <= n:
         while n % i == 0:
-            res += i
-            n //= i
+            res += i #Add the prime factor i to the result
+            n //= i # Divide n by i to reduce it
 
-        i += 1
+        i += 1 # Move on to the next potential prime factor
 
     if n > 1:
-        res += n
+        res += n # If n is greater than 1 after the loop, add it to the result
+    '''
+    i = 2  # Start with the smallest potential divisor, which is 2
+    while i <= n:
+        while n % i == 0:
+            res += i
+            n = n // i  # Divide 'n' by 'i' to update it
+
+        i += 1
 
     return res
