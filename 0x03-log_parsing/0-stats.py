b2471214b84e5dc3fd1b457cea67c33c95ca614d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-19 22:10:41 +0100

    trying

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,36 @@
 #!/usr/bin/python3
+''' module for log parsing '''
+
+
 import sys
-import signal
+total = 0
+counter = 0
+sc_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+           '403': 0, '404': 0, '405': 0, '500': 0}
 
-# Define the status codes to track
-status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
 
-# Initialize variables to store statistics
-line_count = 0
-total_file_size = 0
-status_code_counts = {code: 0 for code in status_codes}
+def print_data(total):
+    ''' function to print statistics for input '''
+    print('File size: {}'.format(total))
+    for key, value in sorted(sc_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
-def print_statistics():
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_codes, key=int):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
-
-def process_line(line):
-    global line_count, total_file_size, status_code_counts
-    
-    # Split the line into components
-    parts = line.split()
-    
-    if len(parts) != 7:
-        return  # Skip lines with incorrect format
-    
-    ip, date, request, status_code, file_size = parts
-    
-    if not status_code.isdigit() or status_code not in status_codes:
-        return  # Skip lines with invalid status code
-    
-    line_count += 1
-    total_file_size += int(file_size)
-    status_code_counts[status_code] += 1
-
-    # Print statistics after every 10 lines
-    if line_count % 10 == 0:
-        print_statistics()
-
-def signal_handler(sig, frame):
-    print_statistics()
-
-# Register the signal handler for CTRL+C
-signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        process_line(line)
-except KeyboardInterrupt:
+        rline = line.split(" ")
+        if len(rline) > 4:
+            code = rline[-2]
+            if code in sc_dict.keys():
+                sc_dict[code] += 1
+            filesize = int(rline[-1])
+            total += filesize
+            counter += 1
+        if counter == 10:
+            counter = 0
+            print_data(total)
+except Exception as ex:
     pass
-
-# Print final statistics
-print_statistics()
-
+finally:
+    print_data(total)
