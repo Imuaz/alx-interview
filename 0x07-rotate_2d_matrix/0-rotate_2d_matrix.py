2c6701f0c2c33d2532008ffd5b67d0e919478ea3
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-13 21:41:10 +0100

    rotate 2D matrix function implemented

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,7 +2,7 @@
 
 
 def rotate_2d_matrix(matrix):
-    left,right = 0, len(matrix) - 1
+    left, right = 0, len(matrix) - 1
 
     while left < right:
         for i in range(right - left):
@@ -10,17 +10,32 @@ def rotate_2d_matrix(matrix):
             # save the top left value
             top_left = matrix[top][left + i]
 
-            # move bottom left in to top
+            # move bottom left in to top left
             matrix[top][left + i] = matrix[bottom - i][left]
 
             # move bottom right into bottom left
             matrix[bottom - i][left] = matrix[bottom][right - i]
 
-            #move top right into bottom right
+            # move top right into bottom right
             matrix[bottom][right - i] = matrix[top + i][right]
 
-            #move top left into top right
+            # move top left into top right
             matrix[top + i][right] = top_left
         right -= 1
         left += 1
-            
+
+
+# alternatively
+'''
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+'''
