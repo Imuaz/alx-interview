3427686086a0fc83ac1c732cbf94fb4e141664b8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-15 15:20:07 +0100

    fix some check errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+'''
+Rotate 2D matrix Module
+'''
 
 
 def rotate_2d_matrix(matrix):
+    '''Rotates a 2D matrix in-place'''
     top, left = 0, 0
     bottom, right = len(matrix) - 1, len(matrix[0]) - 1
 
