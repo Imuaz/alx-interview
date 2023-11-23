1a7964bfbd29db67360add571986e10c65b86644
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-23 14:54:38 +0100

    makingChange function implemented

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,22 +10,14 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    '''Initialize an array to store the minimum number of coins needed for
-    each amount from 0 to total
-    '''
+    
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Iterate through each coin value
     for coin in coins:
-        # Update the dp array for each amount from the coin value to the total
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    '''If the value at dp[total] is still infinity, it means the total cannot
-    be met
-    '''
     if dp[total] == float('inf'):
         return -1
     else:
