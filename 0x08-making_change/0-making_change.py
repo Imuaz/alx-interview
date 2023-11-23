8b6304c3ff8cbcdff640900576bac022e7e9e0c3
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-23 15:10:43 +0100

    makingChange function implemented

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,21 @@ Coin change Module
 
 
 def makeChange(coins, total):
-    """Determine the fewest number of coins needed to meet a given amount total
     """
-    if total < 0:
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
+    if total <= 0:
         return 0
+    if coins is None or coins == []:
+        return -1
 
     """Initialize an array to store the minimum number of coins needed for
-    each amount from 0 to total
-    """
+    each amount from 0 to total"""
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
@@ -23,8 +30,7 @@ def makeChange(coins, total):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     """If the value at dp[total] is still infinity, it means the total
-    cannot be met
-    """
+    cannot be met"""
     if dp[total] == float('inf'):
         return -1
     else:
